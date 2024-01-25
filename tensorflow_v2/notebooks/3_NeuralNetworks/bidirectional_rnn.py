d34bf659229a7a7f931f75e3d39a1e63d3192c3a
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2024-01-24 22:25:21 -0500

    Add @tf.function.

--- tensorflow_v2/notebooks/3_NeuralNetworks/bidirectional_rnn.py
+++ tensorflow_v2/notebooks/3_NeuralNetworks/bidirectional_rnn.py
@@ -90,6 +90,7 @@ class BiRNN(Model):
         self.out = layers.Dense(num_classes)
 
     # Set forward pass.
+    @tf.function
     def call(self, x, is_training=False):
         x = self.bi_lstm(x)
         x = self.out(x)
@@ -108,6 +109,7 @@ birnn_net = BiRNN()
 # %%
 # Cross-Entropy Loss.
 # Note that this will apply 'softmax' to the logits.
+@tf.function
 def cross_entropy_loss(x, y):
     # Convert labels to int 64 for tf cross-entropy function.
     y = tf.cast(y, tf.int64)
@@ -117,6 +119,7 @@ def cross_entropy_loss(x, y):
     return tf.reduce_mean(loss)
 
 # Accuracy metric.
+@tf.function
 def accuracy(y_pred, y_true):
     # Predicted class is the index of highest score in prediction vector (i.e. argmax).
     correct_prediction = tf.equal(tf.argmax(y_pred, 1), tf.cast(y_true, tf.int64))
@@ -127,6 +130,7 @@ optimizer = tf.optimizers.Adam(learning_rate)
 
 # %%
 # Optimization process.
+@tf.function
 def run_optimization(x, y):
     # Wrap computation inside a GradientTape for automatic differentiation.
     with tf.GradientTape() as g:
