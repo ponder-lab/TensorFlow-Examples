c09baa0f32a1081e9a4ac736bd5d55b79f113105
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2024-01-25 16:14:36 -0500

    Add metrics to new test.

--- tensorflow_v2/notebooks/3_NeuralNetworks/convolutional_network.py
+++ tensorflow_v2/notebooks/3_NeuralNetworks/convolutional_network.py
@@ -1,4 +1,7 @@
 from __future__ import absolute_import, division, print_function
+from scripts.utils import write_csv
+import timeit
+
 # %%
 """
 # Convolutional Neural Network Example
@@ -62,6 +65,9 @@ x_train, x_test = x_train / 255., x_test / 255.
 # Use tf.data API to shuffle and batch data.
 train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train))
 train_data = train_data.repeat().shuffle(5000).batch(batch_size).prefetch(1)
+
+start_time = timeit.default_timer()
+skipped_time = 0
 
 # %%
 # Create TF Model.
@@ -149,6 +155,12 @@ def run_optimization(x, y):
     # Update W and b following gradients.
     optimizer.apply_gradients(zip(gradients, trainable_variables))
 
+total_loss = 0
+loss_count = 0
+
+total_accuracy = 0
+accuracy_count = 0
+
 # %%
 # Run training for the given number of steps.
 for step, (batch_x, batch_y) in enumerate(train_data.take(training_steps), 1):
@@ -158,13 +170,21 @@ for step, (batch_x, batch_y) in enumerate(train_data.take(training_steps), 1):
     if step % display_step == 0:
         pred = conv_net(batch_x)
         loss = cross_entropy_loss(pred, batch_y)
+        total_loss += loss
+        loss_count += 1
         acc = accuracy(pred, batch_y)
+        total_accuracy += acc
+        accuracy_count += 1
+        print_time = timeit.default_timer()
         print("step: %i, loss: %f, accuracy: %f" % (step, loss, acc))
+        skipped_time += timeit.default_timer() - print_time
 
 # %%
 # Test model on validation set.
 pred = conv_net(x_test)
+print_time = timeit.default_timer()
 print("Test Accuracy: %f" % accuracy(pred, y_test))
+skipped_time += timeit.default_timer() - print_time
 
 # %%
 # Visualize predictions.
@@ -176,8 +196,14 @@ n_images = 5
 test_images = x_test[:n_images]
 predictions = conv_net(test_images)
 
+time = timeit.default_timer() - start_time - skipped_time
+avg_loss = float(total_loss) / float(loss_count)
+avg_accuracy = float(total_accuracy)/ float(accuracy_count)
+
 # Display image and model prediction.
 for i in range(n_images):
     plt.imshow(np.reshape(test_images[i], [28, 28]), cmap='gray')
     plt.show()
     print("Model prediction: %i" % np.argmax(predictions.numpy()[i]))
+
+write_csv(__file__, training_steps, avg_accuracy, avg_loss, time)
